e851d7bb34a1972ec3a5126a431248c5e8815319
allenbakki <reshma2001d@gmail.com>
2024-05-02 09:41:47 +0530

    integrated the job list api in frontend

---
+++ src/api/jobList.js
@@ -0,0 +1,29 @@
+async function getJobListData(offset) {
+  try {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      limit: 10,
+      offset: { offset },
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: { raw },
+    };
+
+    const response = await fetch(
+      `https://api.weekday.technology/adhoc/getSampleJdJSON`,
+      requestOptions
+    );
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default getJobListData;
